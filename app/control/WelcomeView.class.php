1e8047cd5615b2007b1873bf996d43fd670f8111
Augusto César da Costa Marques <gtcesar@hotmail.com>
2020-03-06 19:14:12 -0300

    mudancas com tentry e jquery

--- app/control/WelcomeView.class.php
+++ app/control/WelcomeView.class.php
@@ -1,6 +1,7 @@
 <?php
 
 use Adianti\Widget\Base\TElement;
+use Adianti\Widget\Form\TEntry;
 
 class WelcomeView extends TPage
 {
@@ -10,40 +11,43 @@ class WelcomeView extends TPage
         
         //collapse script
         TScript::create('
-        var x = document.getElementById("coords");
-        
         function getLocation() {
-          if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition);
-          } else { 
-            x.innerHTML = "Geolocation is not supported by this browser.";
+          if ("geolocation" in navigator){             
+            navigator.geolocation.getCurrentPosition(function(position){ 
+                $("#coords").html("Latitude : "+position.coords.latitude+" </br>Longitude :"+ position.coords.longitude);
+                $("[name=latitude]").val(position.coords.latitude);
+                $("[name=longitude]").val(position.coords.longitude);
+              });
+          }else{
+            console.log("O navegador não suporta geolocalização!");
           }
-        }
-        
-        function showPosition(position) {
-          x.innerHTML = "Latitude: " + position.coords.latitude + 
-          "<br>Longitude: " + position.coords.longitude;
         }
 ');
 
+        $latidude = new TEntry('latitude');
+        $longitude = new TEntry('longitude');
 
         $p = new TElement('p');
         $p->id = 'coords';
 
+        $div = new TElement('div');
+        $div->add($p);
+        $div->add($latidude);
+        $div->add($longitude);
 
         $vbox = new TVBox;       
         
         $botao = new TButton('botao');
         $botao->setImage('fa:map-marker green');        
-        $botao->setLabel('Minhas coordenadas');        
-        $botao->addFunction("getLocation()");
+        $botao->setLabel('Minhas coordenadas');     
+        $botao->addFunction("getLocation()");   
         
         $hbox = new THBox;
         $hbox->addRowSet( $botao );
         $frame = new TFrame;
         $frame->setLegend('Clique para obter as suas coordenadas');
         $frame->add($hbox);
-        $frame->add($p);
+        $frame->add($div);
         
         $vbox->add($frame);
         
